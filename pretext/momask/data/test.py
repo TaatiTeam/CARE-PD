ef0cbab7636c15d85588a9e79c2de7a6bba9ed7d
rajabi2001 <mj.rajabi2001@gmail.com>
2025-05-15 19:22:39 -0400

    initiate pretext eval

---
+++ pretext/momask/data/test.py
@@ -0,0 +1,17 @@
+from os.path import join
+import torch
+from torch.utils import data
+import numpy as np
+from tqdm import tqdm
+from torch.utils.data._utils.collate import default_collate
+import random
+import codecs as cs
+
+import re
+from collections import defaultdict
+
+
+if __name__ == '__main__':
+
+    print("tets")
+    exit()
